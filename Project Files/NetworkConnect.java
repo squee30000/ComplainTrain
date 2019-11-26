00f1aa9d84e862d7aa96196532df24aabc115471
Haustkottr <jakob.struble@gmail.com>
2019-11-26 12:47:21 -0600

    fixed DAO

--- Project Files/NetworkConnect.java
+++ Project Files/NetworkConnect.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-
-import java.io.IOException;
-
-import java.io.InputStreamReader;
+import java.io.*;
 
 import java.net.InetSocketAddress;
 
@@ -10,21 +6,19 @@ import java.net.Socket;
 
 import javax.swing.JOptionPane;
 
-import //ComplaintObject
-
 public class NetworkConnect{
 	private Socket s;
 	private OutputStream outputStream;
 	private ObjectOutputStream objOutputStream;
 	
-	public NetworkConnect(){
+	public NetworkConnect() throws IOException {
 		s = new Socket("localhost", 7070); //Needs to be changed to connect to database
 		outputStream = s.getOutputStream();
 		objOutputStream = new ObjectOutputStream(outputStream);	
 		
 	}
 	
-	public void sendObject(ComplaintObject complaint){
+	public void sendObject(ComplaintObject complaint) throws IOException {
 		objOutputStream.writeObject(complaint);
 	}
 }
